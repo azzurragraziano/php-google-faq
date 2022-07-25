de83d4f4bc2e33e6c667a8e5e443eb92dc76b774
azzurragraziano <azzurragraziano02@email.com>
2022-07-25 17:22:19 +0200

    done

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             'question' =>"Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
             'answer' => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.<br><br>
             Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.<br><br>
-            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
+            Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.<br><br>
             Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
         ],
         [
@@ -22,6 +22,29 @@
         [
             'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
             'answer' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
+        ]
+    ];
+
+    $headerMenu = [
+        [
+            'url' => '#',
+            'link' => 'Introduzione'
+        ],
+        [
+            'url' => '#',
+            'link' => 'Norme sulla privacy'
+        ],
+        [
+            'url' => '#',
+            'link' => 'Termini di servizio '
+        ],
+        [
+            'url' => '#',
+            'link' => 'Tecnologie'
+        ],
+        [
+            'url' => '#',
+            'link' => 'Domande frequenti'
         ]
     ];
 ?>
@@ -39,21 +62,44 @@
 <body>
    
     <header>
+        <!-- logo -->
+        <div>
+            <div class="logo">
+                <img src="./img/google.png" alt="logo">
+            </div>
 
+            <h2>Privacy&Termini</h2>
+        </div>
+
+        <!-- navigation area -->
+        <nav>
+            <!-- links list -->
+            <ul>
+                <?php foreach($headerMenu as $links) { ?>
+                    <li>
+                        <a href="<?php echo $links['url']; ?>">
+                            <?php echo $links['link']; ?>
+                        </a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </nav>
     </header>
 
     <main>
-        <?php foreach($questions as $QandA) { ?>
-            <div>
-                <h2>
-                    <?php echo $QandA['question']; ?>
-                </h2>
+        <div class="container">
+            <?php foreach($questions as $QandA) { ?>
+                <div class="question_answer">
+                    <h3>
+                        <?php echo $QandA['question']; ?>
+                    </h3>
 
-                <p>
-                    <?php echo $QandA['answer']; ?>
-                </p>
-            </div>
-        <?php } ?>
+                    <p>
+                        <?php echo $QandA['answer']; ?>
+                    </p>
+                </div>
+            <?php } ?>
+        </div>
     </main>
 
 </body>
